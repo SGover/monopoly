8a51a5b8181f3fff4b44baa69462d569277f8a3d
SGover <SGovertime@gmail.com>
2013-11-04 12:50:40 +0200

    added comments

--- gameClasses.py
+++ gameClasses.py
@@ -1,3 +1,5 @@
+
+#this class represents a deck of cards like surprize cards or punishment cards
 class deck():
     def __init__(self,cards):
         self.cards=cards
@@ -12,27 +14,38 @@ class deck():
         card=deck.pop
         discard.append(card)
         return card
+    
+#represents a block on the board that containing an asset
+#this block can belong to a player    
 class assetBlock():
     def __init__(self,asset):
         self.asset=asset
         self.owner=None
     def purchase(self,name):
         self.owner=name
+        
+#represent a block on the board that landing on means u need to pull a card from some deck        
 class cardBlock():
     def __init__(self,deck):
         self.deck=deck
     def getCard():
         self.deck.getCard()
+        
+#represents an asset
+#asset have a name a group name and a value       
 class asset():
-    def __init__(self,name,fatherName,value):
+    def __init__(self,name,groupName,value):
         self.name=name
-        self.fatherName=fatherName
-        self.value=value        
+        self.groupName=groupName
+        self.value=value
+#represent a player
+#player attributes : name,money,location,assets        
 class player():
     def __init__(self,name,money):
         self.name=name
         self.money=money
         self.assets={}
+        self.location=0
     def buy(self,asset):
         self.money-=asset.value
         if(assets.has_key(asset.fatherName)):
