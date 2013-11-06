df5a7c6817b0a445a8bcc9d8c90c2c2cd524de92
Adeel <adeeltariqagain@gmail.com>
2013-11-06 18:17:53 +0500

    Adeel 11/6/13 main class

--- mainGame.py
+++ mainGame.py
@@ -5,12 +5,13 @@ FINISH='finish'
 class monoGame():
     gameState=START
     curr_turn=0
-    def __init__(self,players,board,surpriseDeck,chanceDeck):
+    players = []
+    def __init__(self,players,board,console,statusWindow):
         self.players=players
         self.board=board
         self.surpriseDeck=surpriseDeck
         self.chanceDeck=chanceDeck
-    def doMove(diceSum):
+    def do_move(diceSum):
         player=self.players[curr_turn]        
         currBlock=self.board[(player.location+diceSum)%(len(board)-1)]
         currBlock.landOn(player)
@@ -21,4 +22,17 @@ class monoGame():
             self.chooseFromOptions(actions)
         self.curr_turn=(self.curr_turn+1)%(len(players)-1)
     
-        
+    def is_complete(self):  # check if anyone wins, 
+        c = 0
+        for p in player :
+            if not p.is_bankrupt():
+                c+=1
+                
+        if c == 1:
+            for p in player :
+                if p.is_bankrupt():
+                    #this player wins 
+                    pass
+        elif c == 0:
+            #no one wins 
+            pass
