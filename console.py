b9c319667ecee572245188175dc975cf078cf01e
Adeel <adeeltariqagain@gmail.com>
2013-11-06 20:25:47 +0500

    Adeel 11/6/13 updates

--- console.py
+++ console.py
@@ -3,11 +3,15 @@ class console():
     def __init__(self):
         pass
         
-    def display(self):
-        pass
+    def display(self, string):
+        print(string)
      
     def start(self):
-         print("***************\nNew Game of Monopoly!\n***************")
+        print("***************\nNew Game of Monopoly!\n***************")
      
     def get_player_name(self):
-         return input("Enter the Player name :_ ");   
+        return input("Enter the Player name :_ ");
+    
+    def show_winner(self, winner):
+        print(winner,"is the winner!")
+        
--- mainGame.py
+++ mainGame.py
@@ -1,10 +1,13 @@
 from gameClasses import *
 from console import *
 from statusWindow import *
-DEFAULT_MONEY = 1500
+from random import *
+
+
 START='start'
 INGAME='ingame'
 FINISH='finish'
+
 class monoGame():
     gameState=START
     curr_turn=0
@@ -13,39 +16,64 @@ class monoGame():
         self.board=board
         self.console=console()
         self.statusWindow=statusWindow()
-    def do_move(diceSum):
-        player=self.players[curr_turn]        
-        currBlock=self.board[(player.location+diceSum)%(len(board)-1)]
-        currBlock.landOn(player)
-        actions=currBlock.getActions()
-        if(len(actions)==1):
-            actions[0]()
-        else:
-            self.chooseFromOptions(actions)
-        self.curr_turn=(self.curr_turn+1)%(len(players)-1)
+        self.default_money = 1500
+        self.current_player = 0 # index of the current player
+        self.winner = -1;
+        
+#     def do_move(diceSum):
+#         player=self.players[curr_turn]        
+#         currBlock=self.board[(player.location+diceSum)%(len(board)-1)]
+#         currBlock.landOn(player)
+#         actions=currBlock.getActions()
+#         if(len(actions)==1):
+#             actions[0]()
+#         else:
+#             self.chooseFromOptions(actions)
+#         self.curr_turn=(self.curr_turn+1)%(len(players)-1)
         
     def start(self):
         self.console.start()
         name_player = self.console.get_player_name()
-        new_player1 = player(name_player,DEFAULT_MONEY)
+        new_player1 = player(name_player,self.default_money)
         self.players.append(new_player1)
         
         name_player = self.console.get_player_name()
-        new_player2 = player(name_player,DEFAULT_MONEY)
+        new_player2 = player(name_player,self.default_money)
         self.players.append(new_player2)
+        self.current_player = randrange(len(self.players))
+        
+        while not self.is_complete():
+            self.next_turn()
+            
+        if not winner == -1:
+            console.show_winner(winner)
+    
+    def next_turn(self):
+        # main game logic
+        name = self.players[self.current_player].name
+        console.display(name)
+        #complete the turn than change to next player
+        self.current_player = self.next_player(self.current_player)
         pass
+    
+    def next_player(self, index):
+        if index < len(self.players)-1:
+            index += 1
+        elif index ==  len(self.players)-1:
+            index = 0
+        return index
     
     def is_complete(self):  # check if anyone wins, 
         c = 0
-        for p in player :
+        w = 0
+        for p in self.players :
             if not p.is_bankrupt():
                 c+=1
+                w = p
         if c == 1:
-            for p in player :
-                if p.is_bankrupt():
-                    gameState=FINISH
-                    return True
-                     
+            winner = w
+            gameState=FINISH
+            return True
         elif c == 0:
             gameState=FINISH
             return True
