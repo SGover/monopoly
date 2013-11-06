61b408d407281494a34e0f3a1d2fc05125c489d9
Adeel <adeeltariqagain@gmail.com>
2013-11-06 18:27:11 +0500

    Adeel 11/6/2013 new class

---
+++ console.py
@@ -0,0 +1,8 @@
+class console():
+    
+    def __init__(self):
+        pass
+        
+    def display(self):
+        pass
+        
--- mainGame.py
+++ mainGame.py
@@ -6,11 +6,11 @@ class monoGame():
     gameState=START
     curr_turn=0
     players = []
-    def __init__(self,players,board,console,statusWindow):
+    def __init__(self,board,console,statusWindow,players=players):
         self.players=players
         self.board=board
-        self.surpriseDeck=surpriseDeck
-        self.chanceDeck=chanceDeck
+        self.console=console
+        self.statusWindow=statusWindow
     def do_move(diceSum):
         player=self.players[curr_turn]        
         currBlock=self.board[(player.location+diceSum)%(len(board)-1)]
@@ -21,18 +21,22 @@ class monoGame():
         else:
             self.chooseFromOptions(actions)
         self.curr_turn=(self.curr_turn+1)%(len(players)-1)
+        
+    
     
     def is_complete(self):  # check if anyone wins, 
         c = 0
         for p in player :
             if not p.is_bankrupt():
                 c+=1
-                
         if c == 1:
             for p in player :
                 if p.is_bankrupt():
-                    #this player wins 
-                    pass
+                    gameState=FINISH
+                    return True
+                     
         elif c == 0:
-            #no one wins 
-            pass
+            gameState=FINISH
+            return True
+        gameState=INGAME
+        return False    
